d5c9193120a1854961afeba8d75538c5dcf43c7d
Brijnandan <brijnandan.meena@unthinkable.co>
2025-03-25 13:35:30 +0530

    unit test case for login implemented

--- src/modules/user/test/controllers/login.test.js
+++ src/modules/user/test/controllers/login.test.js
@@ -1,49 +1,71 @@
-const express = require('express');
-const userRoutes = require('../../routes');
-const UserService = require('../../service/userService');
+
+// nock → Used to mock external API calls.
+// supertest → Helps simulate API requests to test Express routes.
+const nock = require('nock');
+const request = require('supertest');
+//jo jo real me use kiya usse import kro first
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const { generateTokens } = require('../../controllers/userController');
+const UserService = require('../../../user/service/userService');
+const redisClient = require('../../../../../utils/redisClient');
+const {getRolePermissions} = require('../../../roles/service/assignRolePermissionService');
+const {getUserRoles} = require('../../../user/service/userRoleService');
 
-jest.mock('../../service/userService');
-jest.mock('../../../../../utils/redisClient');
+//jaha jaha se jo real me use kiya bo aaya h usse mock kro 
+jest.mock('../../../user/service/userService');
+jest.mock('../../../user/service/userRoleService');
+jest.mock('../../../roles/service/assignRolePermissionService');
+jest.mock('jsonwebtoken');
 jest.mock('bcrypt');
-jest.mock('../../controllers/userController');
-jest.mock('axios');
+jest.mock('../../../../../utils/redisClient');
 
-const app = express();
-app.use(express.json());
-app.use('/api/users', userRoutes);
+const user = {
+  userId: 'd549b39b-3878-4bbb-9f4c-5ba5c0064672',
+  username: 'testUser',
+  useremail: 'test@user.com',
+  isEmailVerified: true,
+  isActive: true,
+  is_verified: true
+};
 
-describe('User Login Controller', () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
-  it('should print on console', () => {
-    // eslint-disable-next-line no-console
-    console.log('34567890');
-  });
-  it('should return 200 with login successfull message', async () => {
-    // const tempUser = { identifier: 'Brij', password: 'Brij@123456' };
-    const userDetails = {
-      userId: '5403ffdb-1ee6-417b-89b5-7df99438c1e8',
-      username: 'Brij',
-      useremail: 'test@yopmail.com',
-      password: '$2b$10$gExqK1J.bgiT0BK8qcwkO.b3OPEEJMYg4.HcmI2icTeng.9e2RQIm',
-      isEmailVerified: true,
-      isActive: true,
-      created_at: '2025-03-10T10:43:09.973Z',
-      updated_at: '2025-03-10T10:43:09.973Z',
-      createdBy: null,
-      modifiedBy: null,
-      is_verified: true,
-    };
+const token = {
+  accessToken: 'testaccesstoken',
+  refreshToken: 'testrefreshtoken',
+};
 
-    UserService.getUsersByEmailOrUserName.mockResolvedValue(userDetails);
+const roles = ['admin', 'user'];
+const permissions = ['read', 'write'];
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+describe('loginUser', () => {
+  it.only('should login successfully with valid credentials', async () => {
+    const req = { body: { identifier: 'testUser', password: 'test@12345' } };
+
+    //mock nactual function result
+    UserService.getUsersByEmailOrUserName.mockResolvedValue(user);
     bcrypt.compare.mockResolvedValue(true);
-    generateTokens.mockResolvedValue({
-      accessToken: 'access_token',
-      refreshToken: 'refresh_token',
-    });
-    // axios.post('http://localhost:3000/user/login').mockResolvedValue
+    jwt.sign.mockReturnValueOnce(token.accessToken).mockReturnValueOnce(token.refreshToken);
+    getUserRoles.mockReturnValue(roles);
+    getRolePermissions.mockResolvedValue(permissions);
+    UserService.getUserPermissions.mockResolvedValue(permissions);
+    redisClient.SET.mockResolvedValue();
+
+    //here intercepts the API request made by supertest and returns a fake response.
+    nock('http://localhost:3000')
+      .post('/user/login')
+      .reply(200, { message: 'Login successful', token });
+
+    //make api call
+    await request('http://localhost:3000')
+      .post('/user/login')
+      .send(req.body)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toHaveProperty('message', 'Login successful');
+        expect(response.body).toHaveProperty('token');
+      });
   });
 });
